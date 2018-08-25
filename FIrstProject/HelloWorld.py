64f2939dbf974b80adb51f534338d249b4f70d35
JulianHoyos <jahoyors@unal.edu.co>
2018-08-25 23:53:36 +0200

    Fisrt Commit

---
+++ FIrstProject/HelloWorld.py
@@ -0,0 +1,17 @@
+from math import *
+print("Hello World")
+x=3
+y='C'
+print(x)
+
+print(floor(3.5))
+friends=[[[6,[7,9,5]],4],9,5,1]
+
+
+print(friends[0][0][1][2])
+
+def hi(name , age):
+    print("Hello "+name+" " + age)
+
+if not(1):
+    hi(input("Insert your name"),input("Insert your age"))
